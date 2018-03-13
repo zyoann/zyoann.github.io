5722ae4a5594a9472f810127d7471e3836c723ba
zyoann <37337974+zyoann@users.noreply.github.com>
2018-03-13 16:35:06 +0100

    commit1

---
+++ js.js
@@ -0,0 +1,19 @@
+<SCRIPT type="text/javascript">
+function passWord() {
+var testV = 1;
+var pass1 = prompt('Veuillez entrer le mot de passe','');
+while (testV < 3) {
+if (pass1.toLowerCase() == "suede") {
+window.open('https://goo.gl/forms/gFB4twNbeSSE4k262');
+break ;
+}
+else if (pass1.toLowerCase() == "crunch") {
+window.open('https://goo.gl/forms/2NwNzDBcxkgSZbyy2');
+break ;
+}
+testV+=1;
+var pass1 =
+prompt('Mot de passe incorrect. Veuillez réessayer.','');
+}
+}
+</SCRIPT>
