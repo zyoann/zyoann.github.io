59bb4675cfcf79c526def9b495d8b21fd3ff5915
zyoann <37337974+zyoann@users.noreply.github.com>
2018-03-14 14:34:50 +0100

    commit2

--- js.js
+++ js.js
@@ -1,19 +1,19 @@
 <SCRIPT type="text/javascript">
-function passWord() {
-var testV = 1;
-var pass1 = prompt('Veuillez entrer le mot de passe','');
-while (testV < 3) {
-if (pass1.toLowerCase() == "suede") {
-window.open('https://goo.gl/forms/gFB4twNbeSSE4k262');
-break ;
-}
-else if (pass1.toLowerCase() == "crunch") {
-window.open('https://goo.gl/forms/2NwNzDBcxkgSZbyy2');
-break ;
-}
-testV+=1;
-var pass1 =
-prompt('Mot de passe incorrect. Veuillez réessayer.','');
-}
-}
+      function button_click() {
+        window.parent.postMessage(document.getElementById("theMessage").value, "*");
+		alert(document.getElementById("theMessage").value);
+		var testV = 1;
+		var pass1 = document.getElementById("theMessage").value;
+		while (testV < 3) {
+		if (pass1.toLowerCase() == "suede") {
+		window.open('https://goo.gl/forms/gFB4twNbeSSE4k262');
+		break ;
+		}
+		else if (pass1.toLowerCase() == "crunch") {
+		window.open('https://goo.gl/forms/2NwNzDBcxkgSZbyy2');
+		break ;
+		}
+		testV+=1;
+		}
+	}
 </SCRIPT>
